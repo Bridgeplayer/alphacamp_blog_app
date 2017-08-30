9b04ef0418ea80708bcce990e44eddfbb179f52b
Pictionary <opcoys@hotmail.com>
2017-08-30 15:16:21 +0800

    Build Rails app with 3 pages (Landing page, about, and contact pages)

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  #  pages#home 
+  #  pages-> controller
+  #  #home -> views(html)
   root to: 'pages#home'
 
   get 'about', to: 'pages#about'
+
+  get 'contact', to: 'pages#contact'
 end
