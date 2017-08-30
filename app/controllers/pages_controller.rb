5932227f867252d4e696cb57ba5be57b626f4362
Pictionary <opcoys@hotmail.com>
2017-08-30 21:02:03 +0800

    Create a ViewCounter

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,10 +1,31 @@
 class PagesController < ApplicationController
 	def home
+		home_count = Viewcounter.find_by page_name: "Home"
+		if home_count != nil 
+			home_count.view_count += 1
+			home_count.save
+		else
+			Viewcounter.create(view_count: 1, page_name: "Home")
+		end
 	end
 
 	def about
+		about_count = Viewcounter.find_by page_name: "About"
+		if about_count != nil 
+			about_count.view_count += 1
+			about_count.save
+		else
+			Viewcounter.create(view_count: 1, page_name: "About")
+		end		
 	end
 
 	def contact
+		contact_count = Viewcounter.find_by page_name: "Contact"
+		if contact_count != nil 
+			contact_count.view_count += 1
+			contact_count.save
+		else
+			Viewcounter.create(view_count: 1, page_name: "Contact")
+		end
 	end
 end
